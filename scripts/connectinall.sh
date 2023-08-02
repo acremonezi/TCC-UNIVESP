7e1987e70d55f2a79dce8a47877b933ba19ee10b
2005869 <2005869@aluno.univesp.br>
2023-08-02 13:55:00 -0300

    connect in all hosts with one script

---
+++ scripts/connectinall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gnome-terminal --tab -- bash -c "./connecttcc01.sh; bash"
+gnome-terminal --tab -- bash -c "./connecttcc02.sh; bash"
+gnome-terminal --tab -- bash -c "./connecttcc03.sh; bash"
+gnome-terminal --tab -- bash -c "./connecttcc04.sh; bash"
+gnome-terminal --tab -- bash -c "./connecttcc05.sh; bash"
+gnome-terminal --tab -- bash -c "./connecttcc06.sh; bash"
+gnome-terminal --tab -- bash -c "./connecttcc07.sh; bash"
+gnome-terminal --tab -- bash -c "./connecttcc08.sh; bash"
+
