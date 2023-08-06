04be7092e562a69e9a6487eb6dae6c7bad67e50c
2005869 <2005869@aluno.univesp.br>
2023-08-06 15:48:20 -0300

    add virtual machines

---
+++ docker/TCC/data/ubuntuansible/ubuntuansible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Espera alguns segundos para garantir que os outros contêineres tenham tempo de inicializar
+sleep 10
+
+cp -r data/.ssh root/
+
+apt update
+apt install python3 -y
+apt install python3-pip -y
+apt install openssh-client -y
+python3 -m pip install ansible
+
+ssh-add ~/.ssh/id_rsa
+
+apt update -y
+apt upgrade -y
+
+tail -f /dev/null
